8c5924e916b6933bc2435755a6bf8eb7524e1f08
dylan-dang <endercraft2319@gmail.com>
2023-10-22 05:07:01 -0500

    test

--- app/actions/item.ts
+++ app/actions/item.ts
@@ -2,7 +2,7 @@
 
 import { URLSearchParams } from 'url';
 import { selectors, chromium } from 'playwright';
-const { executablePath } = require('@sparticuz/chromium-min');
+const chrome = require('@sparticuz/chromium-min');
 // const puppeteer = require('puppeteer-core');
 
 export interface Item {
@@ -23,50 +23,25 @@ function parseUrl(base: string, rel: string, params?: ConstructorParameters<type
 
 async function getTargetItem(searchTerm: string): Promise<Item | null> {
     const base = 'https://www.target.com'
-    // const browser = await puppeteer.launch({
-    //     args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-    //     defaultViewport: chromium.defaultViewport,
-    //     executablePath: await chromium.executablePath(
-    //         `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-    //     ),
-    //     headless: chromium.headless,
-    //     ignoreHTTPSErrors: true,
-    // });
-    // const page = await browser.newPage();
-    // await page.goto("https://google.com");
-    // const pageTitle = await page.title();
-    // console.log(pageTitle);
-    // await browser.close();
-    const browser = await chromium.launch({
-        executablePath: await executablePath(
-            `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-        )
-    })
-    browser.close();
-    return null;
+    await selectors.setTestIdAttribute('data-test');
+    const executablePath = await chrome.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+    );
+    const browser = await chromium.launch({ executablePath })
+    const page = await browser.newPage();
+    await page.goto(parseUrl(base, '/s', { searchTerm }));
 
-
-    // await selectors.setTestIdAttribute('data-test');
-    // const executablePath = await chrome.executablePath("https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar");
-    // console.log(executablePath);
-    // const browser = await chromium.launch({
-    //     headless: false,
-    // });
-    // const page = await browser.newPage();
-    // await page.goto(parseUrl(base, '/s', { searchTerm }));
-
-    // const productCardWrapper = page.getByTestId('@web/ProductCard/ProductCardVariantDefault').first();
-    // const href = await productCardWrapper.locator('a').first().getAttribute('href');
-    // const src = await productCardWrapper.locator('img').first().getAttribute('src');
-    // const item: Item = {
-    //     image: src ? parseUrl(base, src, {}) : '',
-    //     title: await productCardWrapper.getByTestId('product-title').innerText(),
-    //     price: await productCardWrapper.getByTestId('current-price').innerText(),
-    //     link: href ? parseUrl(base, href) : '',
-    // };
-    // await browser.close();
-    // return item;
-
+    const productCardWrapper = page.getByTestId('@web/ProductCard/ProductCardVariantDefault').first();
+    const href = await productCardWrapper.locator('a').first().getAttribute('href');
+    const src = await productCardWrapper.locator('img').first().getAttribute('src');
+    const item: Item = {
+        image: src ? parseUrl(base, src, {}) : '',
+        title: await productCardWrapper.getByTestId('product-title').innerText(),
+        price: await productCardWrapper.getByTestId('current-price').innerText(),
+        link: href ? parseUrl(base, href) : '',
+    };
+    await browser.close();
+    return item;
 }
 
 export async function getItem(formData: FormData): Promise<Item | null> {
