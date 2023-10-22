05a601b47322d2b3c66cb00deb14d15015753c02
Jalen Wright <gln28@txstate.edu>
2023-10-21 23:51:35 -0500

    changed api key for target

--- app/api.ts
+++ app/api.ts
@@ -7,7 +7,7 @@ const resultsLimit = 10; // hardcoded limit for demonstration purpose
 export async function getTargetData(term: string) {
   const url = new URL("https://api.redcircleapi.com/request");
   const searchParams = new URLSearchParams({
-    api_key: "7AEE896443E54182B77E3C2F2B631561",
+    api_key: "268DB21B52E04D5EA036CF12C013BC77",
     type: "search",
     search_term: term,
     sort_by: "price_low_to_high",
