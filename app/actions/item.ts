5cfcb3bfef2cb3f015042d71c390d34d38d1d756
dylan-dang <endercraft2319@gmail.com>
2023-10-22 03:36:34 -0500

    remove catch

--- app/actions/item.ts
+++ app/actions/item.ts
@@ -22,27 +22,23 @@ function parseUrl(base: string, rel: string, params?: ConstructorParameters<type
 async function getTargetItem(searchTerm: string): Promise<Item | null> {
     const base = 'https://www.target.com'
 
-    try {
-        await selectors.setTestIdAttribute('data-test');
-        const browser = await chromium.launch({ headless: true, executablePath: process.env.VERCEL && '/vercel/.cache/ms-playwright/chromium-1084' });
-        const page = await browser.newPage();
-        await page.goto(parseUrl(base, '/s', { searchTerm }));
+    await selectors.setTestIdAttribute('data-test');
+    const browser = await chromium.launch({ headless: true, executablePath: process.env.VERCEL && '/vercel/.cache/ms-playwright/chromium-1084' });
+    const page = await browser.newPage();
+    await page.goto(parseUrl(base, '/s', { searchTerm }));
 
-        const productCardWrapper = page.getByTestId('@web/ProductCard/ProductCardVariantDefault').first();
-        const href = await productCardWrapper.locator('a').first().getAttribute('href');
-        const src = await productCardWrapper.locator('img').first().getAttribute('src');
-        const item: Item = {
-            image: src ? parseUrl(base, src, {}) : '',
-            title: await productCardWrapper.getByTestId('product-title').innerText(),
-            price: await productCardWrapper.getByTestId('current-price').innerText(),
-            link: href ? parseUrl(base, href) : '',
-        };
-        await browser.close();
-        return item;
-    } catch (e) {
-        alert(`error: ${e}`);
-        return null;
-    }
+    const productCardWrapper = page.getByTestId('@web/ProductCard/ProductCardVariantDefault').first();
+    const href = await productCardWrapper.locator('a').first().getAttribute('href');
+    const src = await productCardWrapper.locator('img').first().getAttribute('src');
+    const item: Item = {
+        image: src ? parseUrl(base, src, {}) : '',
+        title: await productCardWrapper.getByTestId('product-title').innerText(),
+        price: await productCardWrapper.getByTestId('current-price').innerText(),
+        link: href ? parseUrl(base, href) : '',
+    };
+    await browser.close();
+    return item;
+
 }
 
 export async function getItem(formData: FormData): Promise<Item | null> {
