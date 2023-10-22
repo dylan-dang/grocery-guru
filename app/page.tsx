f008127471185770db7cf1860333a019faeabd3d
Jalen Wright <gln28@txstate.edu>
2023-10-22 02:32:52 -0500

    tab icon and name

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
         <>
             <main className='font-playpen text-white flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#4dff73e6] to-[#15162c]'>
                 <div className='flex items-center'>
+                    <link rel="icon" href="/favicon.ico" />
+                    <title>Grocery Guru</title>
                     <img src="/groceryGuruLogo.png" alt="Grocery Guru" className="w-16 h-16 mr-4 rounded-lg spin" />
                     <h1 className='text-6xl font-pacifico'>Grocery Guru</h1>
                 </div>
