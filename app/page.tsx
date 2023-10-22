257d9620dd842a33fc696bea79aabcb3353f49fa
Jonas <gamerboii8787@gmail.com>
2023-10-22 00:35:41 -0500

    changes subtitle text from "cheapest" to "best"

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <main className='font-playpen text-white flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#4dff73e6] to-[#15162c]'>
                 <h1 className='text-6xl font-pacifico'>Grocery Guru</h1>
                 <h2 className='mt-8 text-xl text-center'>
-                    Your one stop shop to compare and find cheapest groceries around you!
+                    Your one stop shop to compare and find the best grocery deals around you!
                 </h2>
                 <SearchForm />
             </main>
